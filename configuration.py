1eb14cd8f51c9d3c8a07d0555879582339e09963
Andres Ruiz <correo@ejemplo.com>
2024-01-08 13:21:46 -0500

    mi primer commit papus

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://5c301591-25e8-41db-8d62-0f905cb4b249.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+
+# KIT_SEARCH_BY_PRODUCT = "/api/v1/products/kits"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ hola.py
@@ -0,0 +1,18 @@
+# numero = 2
+#
+#
+# def hola():
+#     if numero == 2:
+#         print('hola mundo')
+#
+#
+# assert hola, 'nope'
+
+
+def saludos(saludo):
+    print("wooololo")
+
+
+saludos(5)
+
+assert saludo >= 3, 'nopues'
